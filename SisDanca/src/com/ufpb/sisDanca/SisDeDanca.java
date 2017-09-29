9e1f211dccd3dba6fa725169071821c505848a8b
Word <Word@Andrea>
2017-09-28 23:33:11 -0300

    4commi

--- SisDanca/src/com/ufpb/sisDanca/SisDeDanca.java
+++ SisDanca/src/com/ufpb/sisDanca/SisDeDanca.java
@@ -1,65 +1,81 @@
 package com.ufpb.sisDanca;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import com.ufpb.sisDanca.exception.AlunoJaExisteException;
+import com.ufpb.sisDanca.exception.DancaJaExisteException;
 import com.ufpb.sisDanca.exception.ProfessorJaExistenteException;
 
 public class SisDeDanca {
-	private Map<String, Professor>  professores;
-	private Map<String, Aluno>  alunos;
+	private Map<String, Professor> professores;
+	private Map<String, Aluno> alunos;
 	private Map<String, Danca> dancas;
-	
-	public SisDeDanca(){
-		this.professores= new HashMap<String,Professor>();
-	
-		this.alunos= new HashMap<String, Aluno>();
-		this.dancas= new HashMap<String, Danca>();
+
+	public SisDeDanca() {
+		this.professores = new HashMap<String, Professor>();
+
+		this.alunos = new HashMap<String, Aluno>();
+		this.dancas = new HashMap<String, Danca>();
+
+	}
+
+	public void cadastrarAluno(Aluno aluno) throws AlunoJaExisteException {
+		Aluno a= alunos.get(aluno.getEmail());
+		if(a != null){
+			this.alunos.put(aluno.getEmail(), aluno);
+			
+		}else{
+			
+		throw new AlunoJaExisteException(" O Aluno já exista!");
+		}
+			
+	}
+
+	public void cadastrarProfessor(Professor professor)throws ProfessorJaExistenteException  {
+		Professor p= professores.get(professor.getEmail());
+		if(p !=null){
+			this.professores.put(professor.getEmail(), professor);	
+		}else{
+			throw new ProfessorJaExistenteException(" O professor já existe!");
+			
+		}
 		
 	}
-	public void cadastrarAluno(Aluno aluno){
-		this.alunos.put(aluno.getEmail(), aluno);
-	}
-	public void cadastrarProfessor(Professor professor){
-		this.professores.put(professor.getEmail(), professor);
-	}
-	public void cadastrarDanca(Danca danca){
+
+	public void cadastrarDanca(Danca danca) throws DancaJaExisteException {
+		Danca d= dancas.get(danca.getTipoDanca());
+		if( d!= null){
 		this.dancas.put(danca.getTipoDanca(), danca);
+	
+	}else{
+		throw new DancaJaExisteException(" O professor já existe!");	
 	}
-	public Aluno pesquisaAlunoPorDanca(String nome) throws AlunoJaExisteException {
-		Aluno a = alunos.get(nome.toString());
+		
+	}
+
+	public Aluno pesquisaAlunoPorDanca(String email) throws AlunoJaExisteException {
+		Aluno a = alunos.get(email);
 
 		if (a != null) {
 			return a;
 
 		} else {
-			
-		
+
 			throw new AlunoJaExisteException(" O Aluno já exista!");
 		}
-				
-		}
-		public Professor pesquisaProfessor(String nome) throws ProfessorJaExistenteException {
-			Professor p = professores.get(nome);
 
-			if (p != null) {
-				return p;
-
-			} else {
-				throw new ProfessorJaExistenteException(" O Aluno já exista!");
-		
 	}
- }
+
+	public Professor pesquisaProfessor(String nome) throws ProfessorJaExistenteException {
+		Professor p = professores.get(nome);
+
+		if (p != null) {
+			return p;
+
+		} else {
+			throw new ProfessorJaExistenteException(" O Aluno já exista!");
+
+		}
+	}
 }
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-
-
